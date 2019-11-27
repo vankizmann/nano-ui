13796c1ad76de46907fb4bdd5609220764fe4412
Eduard Kizmann <ekizmann@dievisualisten.com>
2019-11-27 17:05:53 +0100

    *

--- src/index.js
+++ src/index.js
@@ -1,27 +1,49 @@
 
-require('./notification/index');
+let scope = {};
 
-require('./render/index');
-require('./draggable/index');
-require('./loader/index');
-require('./form/index');
-require('./button/index');
-require('./input/index');
-require('./textarea/index');
-require('./select/index');
-require('./cascader/index');
-require('./datepicker/index');
-require('./timepicker/index');
-require('./transfer/index');
-require('./file/index');
-require('./matrix/index');
-require('./popover/index');
-require('./modal/index');
-require('./confirm/index');
-require('./table/index');
-require('./paginator/index');
-require('./checkbox/index');
-require('./tabs/index');
-require('./info/index');
-require('./file-list/index');
-require('./wysiwyg/index');
+if (typeof global !== 'undefined') {
+    scope = global;
+}
+
+if (typeof window !== 'undefined') {
+    scope = window;
+}
+
+export function Install(Vue)
+{
+    if ( scope.Nano === undefined ) {
+        return console.error('Nano JS is not available in window scope.');
+    }
+
+    require('./notification/index');
+    require('./render/index');
+    require('./draggable/index');
+    require('./loader/index');
+    require('./form/index');
+    require('./button/index');
+    require('./input/index');
+    require('./textarea/index');
+    require('./select/index');
+    require('./cascader/index');
+    require('./datepicker/index');
+    require('./timepicker/index');
+    require('./transfer/index');
+    require('./file/index');
+    require('./matrix/index');
+    require('./popover/index');
+    require('./modal/index');
+    require('./confirm/index');
+    require('./table/index');
+    require('./paginator/index');
+    require('./checkbox/index');
+    require('./tabs/index');
+    require('./info/index');
+    require('./file-list/index');
+    require('./wysiwyg/index');
+}
+
+if ( scope.Vue !== undefined ) {
+    Install(scope.Vue);
+}
+
+export default Install;
