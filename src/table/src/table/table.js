1fdeb0eb2a1d771e40b91f2ee4051770ee533917
Eduard Kizmann <kizmann@protonmail.ch>
2020-09-18 20:19:10 +0200

    *

--- src/table/src/table/table.js
+++ src/table/src/table/table.js
@@ -309,7 +309,7 @@ export default {
         loadingDelay: {
             default()
             {
-                return 10;
+                return 0;
             },
             type: [Number]
         },
