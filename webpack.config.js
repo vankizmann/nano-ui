87fd2c36aa624030e71f18e8ccd63a8e212c2e28
Eduard Kizmann <kizmann@protonmail.ch>
2021-01-29 03:31:20 +0100

    *

--- webpack.config.js
+++ webpack.config.js
@@ -22,13 +22,13 @@ let config = {
     },
     externals: {
         'vue': {
-            root: 'Vue', global: 'Vue', commonjs2: 'vue', commonjs: 'vue', amd: 'vue'
+            root: 'Vue', commonjs2: 'vue', commonjs: 'vue', amd: 'vue'
         },
         'moment': {
-            root: 'moment', global: 'moment', commonjs2: 'moment', commonjs: 'moment', amd: 'moment'
+            root: 'moment', commonjs2: 'moment', commonjs: 'moment', amd: 'moment'
         },
         '@kizmann/pico-js': {
-            root: 'pi', global: 'pi', commonjs2: '@kizmann/pico-js', commonjs: '@kizmann/pico-js', amd: '@kizmann/pico-js'
+            root: 'pi', commonjs2: '@kizmann/pico-js', commonjs: '@kizmann/pico-js', amd: '@kizmann/pico-js'
         }
     },
     plugins: []
@@ -90,13 +90,13 @@ module.exports = function (env, argv) {
             path: path.resolve(__dirname, "dist"),
             library: "nano",
             libraryTarget: "var",
+        },
+
+        externals: {
+            'vue': 'Vue', 'moment': 'moment', '@kizmann/pico-js': 'pi'
         }
 
     }, config);
-
-    Object.keys(globalPackage.externals).forEach((key) => {
-        globalPackage.externals[key] = globalPackage.externals[key].root;
-    });
 
     let stylePackage = Object.assign({
 
