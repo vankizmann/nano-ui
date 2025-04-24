6cd95f27ed6f5d814760780b2af9799786aefe1c
Eduard Kizmann <kizmann@protonmail.ch>
2025-04-24 19:16:46 +0200

    Nothing

--- webpack.config.js
+++ webpack.config.js
@@ -84,9 +84,6 @@ let themeFn = (entry, target) => ({
 
     },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env.SASS_DISABLE_DEPRECATION_WARNINGS': JSON.stringify('true')
-        }),
         new MiniCssExtractPlugin({
             filename: target
         })
